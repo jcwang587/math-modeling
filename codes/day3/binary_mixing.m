17e4fcac6d2b486083c8e9754b8b456d1c208c2d
Jiacheng Wang <kashkingwong@gmail.com>
2025-03-13 11:36:41 -0400

    Create binary_mixing.m

---
+++ codes/day3/binary_mixing.m
@@ -0,0 +1,12 @@
+function f = binary_mixing(x) 
+    w1 = 10;
+    x1 = 0.1;
+    w2 = 2;
+    x2 = 0.9;
+    rhoa = 10;
+    cv = 5;
+    h = x(1);
+    x3 = x(2);
+    f(1) = (w1+w2-cv*sqrt(h))/rhoa;
+    f(2) = (w1*(x1-x3)+w2*(x2-x3))/(rhoa*h);
+end
