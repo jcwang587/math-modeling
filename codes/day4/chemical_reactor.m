0dd918b3e9a7e4e7b8cecfcf411ca4232c51a0a2
Jiacheng Wang <kashkingwong@gmail.com>
2025-03-26 20:22:49 -0400

    Update chemical_reactor.m

--- codes/day4/chemical_reactor.m
+++ codes/day4/chemical_reactor.m
@@ -1,18 +1,22 @@
+%% Clear environment
 clear
 clc
 
+%% Define reaction coefficients
 k1 = 4; 
 k2 = 2; 
 k3 = 5; 
 k4 = 2; 
 k5 = 1;
 
+%% Define control system matrix
 A = [-k1, k2, 0; k1, -(k2+k3+k4), 0; 0, k3, -k5];
 B = [];
 C = eye(3);
 D = [];
 
+%% Solve the system 
 % Install control system toolbox
 sys = ss(A, B, C, D);
 x0 = [10, 0, 0]';
-initialplot(sys, x0)
+initialplot(sys, x0);
