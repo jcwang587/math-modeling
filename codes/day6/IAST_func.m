ea77826fb36af5b5f3e276dda200f113e27ad4c0
Jiacheng Wang <kashkingwong@gmail.com>
2025-04-16 13:15:19 -0400

    fix solution

---
+++ codes/day6/IAST_func.m
@@ -0,0 +1,32 @@
+%% Nonlinear equation systems
+function [err, lnP0, psi] = IAST_func(x, lnP, ads_pot)
+% IAST equations for N-component systems
+%
+% Syntax
+%   y = IAST_func(x, lnP, ads_pot)
+%
+% Description
+%   x(i), 1<=i<=N-1: absorbed-phase mole fraction for the i-th component
+%   x(N-1+i), 1<=i<=N: ln p_i^0
+%   lnP(i), 1<=i<=N: ln(bulk phase pressure)
+
+tol = 1e-5;
+
+N = numel(lnP);
+z = [x(1:N-1), 1-sum(x(1:N-1))];  % x1, x2, ..., xN = 1-x1-x2-...-x(N-1)
+
+err = zeros(1, 2*N-1);
+lnP0 = x(N:2*N-1);
+psi = zeros(1, N);
+
+psi(N) = ads_pot{N}(lnP0(N));
+
+for i = 1:N-1
+    psi(i) = ads_pot{i}(lnP0(i));
+    diff = abs(psi(N) - psi(i));  % Eqn. 2: Pi_i^0 (p_i^0) = Pi_N^0 (p_N^0)
+    err(N+i) = diff/(psi(N)+tol) + diff/(psi(i)+tol);
+end
+
+err(1:N) = exp(lnP-lnP0) - z;  % Eqn. 1: P*y1/P_i^0 - x_i*gamma_i = 0
+
+end
---
+++ codes/day6/main.m
@@ -0,0 +1,96 @@
+%% Load data
+clear;
+clc;
+N = 2;
+unary = cell(2);
+load data.mat
+
+%% Part 1
+semilogx(unary{1}(:,1), unary{1}(:,2), 'bo')
+hold on; semilogx(unary{2}(:,1), unary{2}(:,2), 'r^')
+xlabel('p / [Pa]')
+ylabel('Loading N [molec/uc]')
+
+%% Parts 2 & 3
+isotherm = cell(1, N);
+maxlnP = zeros(1, N);  % For storing the maximum pressure in the isotherm data
+minlnP = zeros(1, N);
+options = optimset('FinDiffType', 'central', 'FunValCheck', 'on', 'MaxFunEvals', 600, 'MaxIter', 100, 'Display', 'off');
+
+for i = 1 : N
+    lnP = log(unary{i}(:,1));
+    maxlnP(i) = max(lnP);
+    [lnP, indices] = sort(lnP);
+    unary{i} = unary{i}(indices, :);
+
+    % Part 2
+    isotherm_pp = griddedInterpolant(lnP, unary{i}(:,2), 'linear');
+    isotherm{i} = @(x)isotherm_pp(x);
+
+    % Part 3
+    [minlnP(i), fval, exitflag, output, jacobian] = fsolve(isotherm{i}, min(lnP), options);
+end
+
+%% Part 3: Plotting
+p_range = linspace(min(minlnP), max(maxlnP), 100);
+hold on
+semilogx(exp(p_range), isotherm{1}(p_range), 'b')
+semilogx(exp(p_range), isotherm{2}(p_range), 'r')
+
+%% Part 4
+figure;
+semilogx(binary(:,2), binary(:,3), 'bo')
+hold on; semilogx(binary(:,2), binary(:,4), 'r^')
+xlabel('p_{EtOH} / [Pa]')
+ylabel('Loading N [molec/uc]')
+
+%% Part 5
+tol = 1e-5;
+ads_pot = cell(1, N);
+for i = 1 : N
+    ads_pot{i} = @(y)integral(isotherm{i}, minlnP(i), y);
+
+    % Alternative analytical implementation using the polynomial coefficients; see the end of this file.
+    % ads_pot{i} = @(y)piecewise_polynomial_ads_pot(y, isotherm_pp, minlnP(i));
+end
+
+%% Parts 6 & 7
+binary_reduced =binary([1, 7, 13], :);
+ndata = size(binary_reduced, 1);
+
+options = optimset('FinDiffType', 'central', 'FunValCheck', 'on', 'MaxFunEvals', 800, 'MaxIter', 100, 'TolFun', 1e-6, 'TolX', 1e-6, 'Display', 'iter');
+z = zeros(ndata, 2*N-1);
+err = zeros(ndata, 2*N-1);
+psi = zeros(ndata, N);
+N_tot = zeros(ndata, 1);
+Z = zeros(ndata, N);
+lnP0 = zeros(ndata, N);
+exitflags = zeros(ndata);
+
+lnP_binary = log(binary_reduced(:,1:2));
+for i = 1:ndata
+    fprintf('\n======Data point %d ======\n', i);
+    % Part 6
+    func = @(x)IAST_func(x, lnP_binary(i, :), ads_pot);
+    lb = [zeros(1, N-1), minlnP];  % First N-1 values are lower bounds for z_i, last N values are lower bounds for lnP_i^0
+    ub = [ones(1, N-1), 2*maxlnP];  % First N-1 values are upper bounds for z_i, last N values are upper bounds for lnP_i^0
+    x0 = [ones(1, N-1) * 1/N, lnP_binary(i, :) - log(1/N)];  % lnP_i^0 = lnP_i - lnz_i
+    [z1, resnorm, residual, exitflags(i), output, lambda, jacobian] = lsqnonlin(func, x0, lb, ub, options);
+
+    % Part 7. Below is written for a general N-component mixture
+    z(i, :) = z1;
+    [err(i, :), lnP0(i, :), psi(i, :)] = func(z1);
+    Z(i, 1:N-1) = z1(1:N-1);
+    Z(i, N) = 1 - sum(Z(i, 1:N-1));
+    for j = 1 : N
+        N_tot(i) = N_tot(i) + Z(i, j) / isotherm{j}(lnP0(i, j));  % Eqn. 3
+    end
+    N_tot(i) = 1 / N_tot(i);
+
+end
+N_predicted = N_tot .* Z;  % Eqn. 4
+
+%% Part 7: Display IAST predicted data
+hold on
+semilogx(binary_reduced(:,2), N_predicted(:, 1), 'bo', 'MarkerFaceColor', 'b')
+semilogx(binary_reduced(:,2), N_predicted(:,2), 'r^',  'MarkerFaceColor', 'r')
